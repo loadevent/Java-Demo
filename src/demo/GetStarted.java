0c07267fe8b0c665547b26b4d7559016cee05713
loadevent <loadevent.academy@gmail.com>
2024-06-24 18:02:44 +0200

    addeded files

---
+++ src/demo/GetStarted.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class GetStarted {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Training again");
+        System.out.println("Monday 24th");
+        System.out.print("Chapter 1");
+        System.out.println();
+
+    }
+}
